3c2d31c203dc836928db913b4a6e421982526a88
Ivan <104781156+IvanTol@users.noreply.github.com>
2022-09-16 08:39:59 +0300

    07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galeryCardItems = createGaleryCardItems(galleryItems);
+
+const galleryContainerRef = document.querySelector(".gallery");
+
+galleryContainerRef.insertAdjacentHTML("beforeend", galeryCardItems);
+
+function createGaleryCardItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+// function galeryLightBox(e){
+
+//     const gallery = new SimpleLightbox('.gallery a', {
+//         captionsData: "alt",
+//         captionPosition: 'bottom',
+//         captionDelay: 250
+//     });
+//     galleryContainerRef.removeEventListener;
+// }
+
+// galleryContainerRef.addEventListener("click", galeryLightBox);
